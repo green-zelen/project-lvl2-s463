e8b12343eda638563e980a60911b67cd1b1a75fa
Maria Perlovskaya <marigreenzelen@gmail.com>
2019-08-27 21:12:31 +0200

    Расстановка пробелов с сортировкой

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import _ from 'lodash';
 import program from 'commander';
 import { getAst } from './genDiff';
 import renderDiff from './renderDiff';
@@ -29,6 +30,15 @@ if (process.argv.slice(2).length === 0) {
 const first = readFile(firstFile);
 const second = readFile(secondFile);
 
-const objectForRender = getAst(first, second);
-const renderResult = renderDiff(objectForRender);
+const ast = getAst(first, second);
+const recSort = (rt) => {
+  rt = rt.map(el =>  _.has(el, 'children') ? { ...el, children: recSort(el.children) } : el);
+  return _.sortBy(rt, el => el.key);
+};
+
+const sortedAst = recSort(ast);
+
+console.log(sortedAst[1].children);
+
+const renderResult = renderDiff(sortedAst);
 console.log(renderResult);
--- src/bin/renderDiff.js
+++ src/bin/renderDiff.js
@@ -1,20 +1,21 @@
-const makeDiff = (diff) => {
+const makeDiff = (diff, depth) => {
+  const rep = ('  '.repeat(depth));
+  const repCh = rep.slice(2);
   const renderMethodes = {
-    equal: (acc, { key, value }) => ({ ...acc, [`  ${key}`]: value }),
-    added: (acc, { key, value }) => ({ ...acc, [`+ ${key}`]: value }),
-    deleted: (acc, { key, preValue }) => ({ ...acc, [`- ${key}`]: preValue }),
+    equal: (acc, { key, value }) => ({ ...acc, [`${rep}${key}`]: value }),
+    added: (acc, { key, value }) => ({ ...acc, [`${repCh}+ ${key}`]: value }),
+    deleted: (acc, { key, preValue }) => ({ ...acc, [`${repCh}- ${key}`]: preValue }),
     changed: (acc, { key, value, preValue }) => ({
       ...acc,
-      [`- ${key}`]: preValue,
-      [`+ ${key}`]: value,
+      [`${repCh}- ${key}`]: preValue,
+      [`${repCh}+ ${key}`]: value,
     }),
-    children: (acc, { key, children }) => ({ ...acc, [`  ${key}`]: makeDiff(children) }),
+    children: (acc, { key, children }) => ({ ...acc, [`${rep}${key}`]: makeDiff(children, depth + 1) }),
   };
-
   const filesDiff = diff.reduce((acc, el) => renderMethodes[el.type](acc, el), {});
   return filesDiff;
 };
 
-const renderDiff = diff => JSON.stringify(makeDiff(diff), null, 2).replace(/"/g, '').replace(/,/g, '');
+const renderDiff = diff => JSON.stringify(makeDiff(diff, 1), null, 2).replace(/"/g, '').replace(/,/g, '');
 
 export default renderDiff;
